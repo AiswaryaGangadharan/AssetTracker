a236b3b52a12fdf6bcefcf46659a826891f9c020
AiswaryaGangadharan <aishzz0123@gmail.com>
2026-04-13 14:16:55 +0530

    fix include_router bug

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
-from routers import auth
-from routers.employees import router as employees
-from routers.assets import router as assets
-from routers.assignments import router as assignments
-from routers.dashboard import router as dashboard
-from routers.search import router as search
+from routers import auth_router as auth
+from routers import employees_router as employees
+from routers import assets_router as assets
+from routers import assignments_router as assignments
+from routers import dashboard_router as dashboard
+from routers import search_router as search
 app = FastAPI(title="Asset Tracker API")
 
-app.include_router(auth.router, prefix="/auth", tags=["Auth"])
-app.include_router(employees.router, prefix="/employees", tags=["Employees"])
-app.include_router(assets.router, prefix="/assets", tags=["Assets"])
-app.include_router(assignments.router, prefix="/assignments", tags=["Assignments"])
-app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
-app.include_router(search.router, prefix="/search", tags=["Search"])
+app.include_router(auth, prefix="/auth", tags=["Auth"])
+app.include_router(employees, prefix="/employees", tags=["Employees"])
+app.include_router(assets, prefix="/assets", tags=["Assets"])
+app.include_router(assignments, prefix="/assignments", tags=["Assignments"])
+app.include_router(dashboard, prefix="/dashboard", tags=["Dashboard"])
+app.include_router(search, prefix="/search", tags=["Search"])
 
 @app.get("/")
 def root():
